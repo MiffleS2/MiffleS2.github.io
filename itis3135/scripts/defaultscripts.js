06383292a563ec1377691b0e95df00f9a9d035f2
MiffleS2 <chanson7@uncc.edu>
2022-02-13 14:52:31 -0500

    new stuff
    new stuff

---
+++ itis3135/scripts/defaultscripts.js
@@ -0,0 +1,50 @@
+function alertUser() {
+  alert("Hey my script is running");
+}
+
+function getDate() {
+
+  var now = new Date();
+
+  var months = ["January", "February", "March", "April", "May", "June"
+    , "July", "August", "September", "October", "November", "December"];
+
+  var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+    "Saturday", "Sunday"];
+
+  var ampm = ["am", "am", "am", "am", "am", "am", "am", "am", "am", "am", "am",
+    "pm", "pm", "pm", "pm", "pm", "pm", "pm", "pm", "pm", "pm", "pm", "pm",
+  ];
+
+
+
+  document.write("Today is: " + now.getHours() + ":" + now.getMinutes()
+    + " " + ampm[now.getHours()] + " on " + days[now.getDay()] + ", " + now.getDate()
+    + " " + months[now.getMonth()] + ", " + now.getFullYear());
+}
+
+function formCheck() {
+  let fname = document.forms["information"]["name"].value;
+  let mood = document.forms["information"]["doing"].value;
+
+  document.getElementById("content").innerHTML = `<h1>The Caramel Horse Hanson Designs, LLC. welcomes you, ${fname}! We\'re glad you are doing ${mood}</h1>`;
+}
+
+function getEmail() {
+  document.write("CaramelHorse@gmail.com");
+}
+function getPhone() {
+  document.write("555-555-5555");
+}
+function tellJoke() {
+  document.write("What's the best thing about Switzerland?.... I don't know, but the flag is a big plus.");
+}
+function companyOwn() {
+  document.write("Christopher Hanson");
+}
+function companyAge() {
+  document.write("2 months");
+}
+
+
+
