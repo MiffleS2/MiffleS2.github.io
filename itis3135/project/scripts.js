1ac6af0fa306e7629d3b06650b75f5de1243118c
MiffleS2 <chanson7@uncc.edu>
2022-04-28 16:00:58 -0400

    jquery plugin carousel
    jquery plugin carousel

--- itis3135/project/scripts.js
+++ itis3135/project/scripts.js
@@ -1,21 +1,4 @@
 $(document).ready(function () {
-  var image, count = 0, cache = [];
-  $("#slides img").each(function () {
-    image = new Image();
-    image.src = $(this).attr("src");
-    cache[count] = image;
-    count++;
-  });
-  count = 0;
-  var nextImage;
-  setInterval(function () {
-    $("#slide").fadeOut(1000, function () {
-      count = (count + 1) % cache.length;
-      nextImage = cache[count];
-      $("#slide").attr("src", nextImage.src).fadeIn(1000);
-    });
-  }, 3000);
-
   $(function () {
     $("#tabs").tabs();
   });
